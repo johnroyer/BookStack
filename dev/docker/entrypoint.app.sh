1037e72650d025ff09528ab81e73e75ebfca7233
Zero <john.royer@gmail.com>
2024-10-29 10:51:20 +0800

    no need wait-for-it anymore
    `depends` in docker-compose.yaml will detect automatically

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -8,7 +8,6 @@ if [[ -n "$1" ]]; then
     exec "$@"
 else
     composer install
-    wait-for-it db:3306 -t 45
     php artisan migrate --database=mysql
     chown -R www-data:www-data storage
     exec apache2-foreground
