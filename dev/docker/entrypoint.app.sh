b2732e74e33bb59c91532bd30a4548831aa13ff1
Zero <john.royer@gmail.com>
2024-11-11 13:36:37 +0800

    Revert "Merge branch 'nginx-phpfpm-mysql-meili' into index-meilisearch"
    This reverts commit 4f3725b2acdfd37bffdd28d33e0db3cd03a854a6, reversing
    changes made to 5b5a2ba147039f7031ca201d2e952bc50a77af16.

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -8,6 +8,7 @@ if [[ -n "$1" ]]; then
     exec "$@"
 else
     composer install
+    wait-for-it db:3306 -t 45
     php artisan migrate --database=mysql
     chown -R www-data:www-data storage
     exec apache2-foreground
