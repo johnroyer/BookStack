cc83c0870c29f50c9319c1b81c0c37e1a7d389b4
Zero <john.royer@gmail.com>
2024-11-15 16:59:42 +0800

    update color of MFA remove buttons

--- resources/views/mfa/parts/setup-method-row.blade.php
+++ resources/views/mfa/parts/setup-method-row.blade.php
@@ -13,7 +13,7 @@
             </div>
             <a href="{{ url('/mfa/' . $method . '/generate') }}" class="button outline small">{{ trans('auth.mfa_setup_reconfigure') }}</a>
             <div component="dropdown" class="inline relative">
-                <button type="button" refs="dropdown@toggle" class="button outline small">{{ trans('common.remove') }}</button>
+                <button type="button" refs="dropdown@toggle" class="button outline small text-warn">{{ trans('common.remove') }}</button>
                 <div refs="dropdown@menu" class="dropdown-menu">
                     <p class="text-neg small px-m mb-xs">{{ trans('auth.mfa_setup_remove_confirmation') }}</p>
                     <form action="{{ url('/mfa/' . $method . '/remove') }}" method="post">
